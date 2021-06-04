247006501053a3c688ce776a000bdd5c86776674
ansilreji <84070903+ansilreji@users.noreply.github.com>
2021-06-04 09:31:39 +0530

    Create wallis.c

---
+++ wallis.c
@@ -0,0 +1,61 @@
+#include <assert.h>
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <math.h>
+
+float wallis_pi(int count)
+
+{
+
+float value=1.0;
+
+for(int a=1;a<=count;a++)
+
+ {
+
+ float num=4*a*a;
+
+ value*=num/(num-1);
+
+ }
+
+return value*2;
+
+}float wallis_pi(int);
+
+int main(void) {
+
+  float pi;
+
+  for (int i=0; i<5; i++) {
+
+    pi = wallis_pi(i);
+
+    if (!(fabs(pi - M_PI) > 0.15)) {
+
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+
+      abort();
+
+    }
+
+  }
+
+  for (int i=500; i<3000; i++) {
+
+    pi = wallis_pi(i);
+
+    if (!(fabs(pi - M_PI) < 0.01)) {
+
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+
+      abort();
+
+    }
+
+  }
+
+}
